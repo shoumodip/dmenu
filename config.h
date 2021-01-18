2a25e605c8f315146423611b78a5b73561e2fc7f
shoumodip <shoumodipkar@gmail.com>
2021-01-18 19:04:30 +0530

    Configured fonts and colors

--- config.h
+++ config.h
@@ -1,28 +1,34 @@
 /* See LICENSE file for copyright and license details. */
-/* Default settings; can be overriden by command line. */
 
+/* Functionality */
 static int instant = 1;
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
-/* -fn option overrides fonts[0]; default X11 font or font set */
+static int topbar  = 1; /* Dmenu appears at top. [-b] */
+static int fuzzy   = 1; /* Dmenu uses fuzzy matching. [-F] */
+
+/* User Interface */
+static const char *prompt       = NULL; /* Prompt to the left of input field. [-p] */
+static unsigned int lines       = 0; /* If nonzero, dmenu uses vertical list with given number of lines. [-l] */
+static unsigned int preselected = 0; /* Preselected item starting from 0. [-n] */
+
+/* Fonts */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"Monospace:pixelsize=18"
 };
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+
+/* Colors */
+static const char col_gray1[] = "#161616";
+static const char col_gray2[] = "#ebdbb2";
+static const char col_green[] = "#4d6e4e";
+
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	/*                  Fg         Bg       */
+	[SchemeNorm]    = { col_gray2, col_gray1 },
+	[SchemeSel]     = { col_gray1, col_green },
+	[SchemeOut]     = { col_gray2, col_gray1 },
 };
-/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
 
 /*
  * Characters not considered part of a word while deleting words
  * for example: " /?\"&[]"
  */
 static const char worddelimiters[] = " ";
-
-/* -n option; preselected item starting from 0 */
-static unsigned int preselected = 0;

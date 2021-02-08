72023d6e9428c64343ef753c4cbbecb4cfd2de0a
shoumodip <shoumodipkar@gmail.com>
2021-02-08 13:56:46 +0530

    Revert "Configured fonts and colors"
    This reverts commit 2a25e605c8f315146423611b78a5b73561e2fc7f.

--- config.h
+++ config.h
@@ -1,37 +1,30 @@
 /* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
 
-/* Functionality */
 static int instant = 1;
-static int topbar  = 1; /* Dmenu appears at top. [-b] */
-static int fuzzy   = 1; /* Dmenu uses fuzzy matching. [-F] */
+static int topbar  = 1; /* Place dmenu on top. [-b] option */
+static int fuzzy   = 1; /* Use fuzzy matching. [-F] option */
 
-/* User Interface */
-static const char *prompt       = NULL; /* Prompt to the left of input field. [-p] */
-static unsigned int lines       = 0; /* If nonzero, dmenu uses vertical list with given number of lines. [-l] */
-static unsigned int preselected = 0; /* Preselected item starting from 0. [-n] */
-
-/* Fonts */
 static const char *fonts[] = {
-	"monospace:pixelsize=15"
+	"Monospace:pixelsize=18:antialias=true:autohint=true"
 };
-
-/* Colors */
-static const char col_fg[] = "#ebdbb2";
-static const char col_bg[] = "#161616";
-static const char col_sl[] = "#080808";
-static const char col_hl[] = "#98971a";
 
 static const char *colors[SchemeLast][2] = {
-	/*                          Fg      Bg    */
-	[SchemeNorm]            = { col_fg, col_bg },
-	[SchemeSel]             = { col_fg, col_sl },
-	[SchemeOut]             = { col_fg, col_bg },
-	[SchemeSelHighlight]    = { col_hl, col_sl },
-	[SchemeNormHighlight]   = { col_hl, col_bg },
+	/*               Fg         Bg       */
+	[SchemeNorm] = { "#ebdbb2", "#161616" },
+	[SchemeSel]  = { "#ebdbb2", "#484848" },
+	[SchemeOut]  = { "#ebdbb2", "#161616" },
 };
+
+static const char *prompt = NULL; /* Prompt. [-p] option */
+
+static unsigned int lines = 0; /* Number of lines in dmenu. [-l] option */
 
 /*
  * Characters not considered part of a word while deleting words
  * for example: " /?\"&[]"
  */
 static const char worddelimiters[] = " ";
+
+/* preselected item starting from 0. [-n] option */
+static unsigned int preselected = 0;
--- dmenu.c
+++ dmenu.c
@@ -27,8 +27,7 @@
 #define TEXTW(X)              (drw_fontset_getwidth(drw, (X)) + lrpad)
 
 /* enums */
-enum { SchemeNorm, SchemeSel, SchemeNormHighlight, SchemeSelHighlight,
-       SchemeOut, SchemeLast }; /* color schemes */
+enum { SchemeNorm, SchemeSel, SchemeOut, SchemeLast }; /* color schemes */
 
 
 struct item {
@@ -118,45 +117,6 @@ cistrstr(const char *s, const char *sub)
 	return NULL;
 }
 
-static void
-drawhighlights(struct item *item, int x, int y, int maxw)
-{
-	int i, indent;
-	char *highlight;
-	char c;
-
-	if (!(strlen(item->text) && strlen(text)))
-		return;
-
-	drw_setscheme(drw, scheme[item == sel
-	                   ? SchemeSelHighlight
-	                   : SchemeNormHighlight]);
-	for (i = 0, highlight = item->text; *highlight && text[i];) {
-		if (tolower(*highlight) == tolower(text[i])) {
-			/* get indentation */
-			c = *highlight;
-			*highlight = '\0';
-			indent = TEXTW(item->text);
-			*highlight = c;
-
-			/* highlight character */
-			c = highlight[1];
-			highlight[1] = '\0';
-			drw_text(
-				drw,
-				x + indent - (lrpad / 2),
-				y,
-				MIN(maxw - indent, TEXTW(highlight) - lrpad),
-				bh, 0, highlight, 0
-			);
-			highlight[1] = c;
-			i++;
-		}
-		highlight++;
-	}
-}
-
-
 static int
 drawitem(struct item *item, int x, int y, int w)
 {
@@ -168,9 +128,7 @@ drawitem(struct item *item, int x, int y, int w)
 	else
 		drw_setscheme(drw, scheme[SchemeNorm]);
 
-	r = drw_text(drw, x, y, w, bh, lrpad / 2, item->text, 0);
-	drawhighlights(item, x, y, w);
-	return r;
+	return drw_text(drw, x, y, w, bh, lrpad / 2, item->text, 0);
 }
 
 static void
@@ -848,7 +806,7 @@ usage(void)
 {
 	fputs("usage: dmenu [-bfinv] [-l lines] [-p prompt] [-fn font] [-m monitor]\n"
 	      "             [-nb color] [-nf color] [-sb color] [-sf color]\n"
-	      "             [-nhb color] [-nhf color] [-shb color] [-shf color] [-w windowid] [-n number]\n", stderr);
+	      "             [-w windowid] [-n number]\n", stderr);
 	exit(1);
 }
 
@@ -893,14 +851,6 @@ main(int argc, char *argv[])
 			colors[SchemeSel][ColBg] = argv[++i];
 		else if (!strcmp(argv[i], "-sf"))  /* selected foreground color */
 			colors[SchemeSel][ColFg] = argv[++i];
-		else if (!strcmp(argv[i], "-nhb")) /* normal hi background color */
-			colors[SchemeNormHighlight][ColBg] = argv[++i];
-		else if (!strcmp(argv[i], "-nhf")) /* normal hi foreground color */
-			colors[SchemeNormHighlight][ColFg] = argv[++i];
-		else if (!strcmp(argv[i], "-shb")) /* selected hi background color */
-			colors[SchemeSelHighlight][ColBg] = argv[++i];
-		else if (!strcmp(argv[i], "-shf")) /* selected hi foreground color */
-			colors[SchemeSelHighlight][ColFg] = argv[++i];
 		else if (!strcmp(argv[i], "-w"))   /* embedding window id */
 			embed = argv[++i];
 		else if (!strcmp(argv[i], "-n"))   /* preselected item */
